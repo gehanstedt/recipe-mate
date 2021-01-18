21e08f8b86cab354fecfd10b19d12085678a7139
Matthews <212689101@ge.com>
2021-01-18 12:51:48 -0500

    updated members.js, members.html, api-routes.js file search working to at least pull back recipes to console.log

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,33 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+
+  // $(document).on("submit", "#searchRecipe", getRecipe);
+  $("#search-bar").on("click", function(event) {
+    // console.log("HERE")
+    // var $newItemInput = $("input.search_input");
+    var $Ingred = $("input.search_input");
+
+  // This function grabs recipes from the web and updates the view
+// function getRecipe() {
+
+  event.preventDefault();
+  var ingredients = {
+    // text: $search.val().trim()
+    text: $Ingred.val().trim()
+  };  
+  console.log(ingredients)
+  // console.log("HERE")
+  $.post("/api/recipe", ingredients) 
+  .done(function(result) {
+    console.log(result)
+    for(var i=0; i < 10; i++){
+    console.log(result[i].recipe.image)
+    console.log(result[i].recipe.label)
+    console.log(result[i].recipe.url)
+    }
+  });
+
+// }
+  });
 });
