732a21b728b2aed1e1df1eaa1d77a3b6abb0581f
drclever <drc0625@aol.com>
2021-01-19 00:45:08 -0500

    Took out unique for name and added validation for email

--- models/user.js
+++ models/user.js
@@ -3,45 +3,47 @@ var bcrypt = require("bcryptjs");
 const { authorize } = require("passport");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    // The email cannot be null, and must be a proper email before creation
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    // The password cannot be null
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-  });
-
-  // Define association of User model to Recipe model
-  User.associate = function (models) {
-    User.hasMany (models.Favorite_recipe, {
-      foreignKey: {
-        name: 'user_id'
-      },
-      onDelete: 'CASCADE'
+    var User = sequelize.define("User", {
+        full_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // The email cannot be null, and must be a proper email before creation
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: {
+                args: true,
+                msg: "Invalid email format"
+              }
+            }
+        },
+        // The password cannot be null
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
     });
-  }
-  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-  });
-  return User;
+
+    // Define association of User model to Recipe model
+    User.associate = function (models) {
+        User.hasMany (models.Favorite_recipe, {
+            foreignKey: {
+                name: 'user_id'
+            },
+              onDelete: 'CASCADE'
+        });
+    }
+    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    User.addHook("beforeCreate", function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+    return User;
 };
