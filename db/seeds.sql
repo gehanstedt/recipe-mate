4801caf572f51a07101f6e51612a4093e5359bc0
Matthews <212689101@ge.com>
2021-01-19 00:36:22 -0500

    added a seeds file that has 10 valid links to build favorites table in the db also modified favorites.handlebars to use link from db for picture

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Buffalo Chicken Potato Skins", "https://food52.com/recipes/40391-buffalo-chicken-potato-skins", "https://www.edamam.com/web-img/d8c/d8cadc29ab605dcbf84dd1ef803418a9.jpeg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Colombian Chicken-Potato Soup", "http://www.cookstr.com/recipes/colombian-chicken-potato-soup", "https://www.edamam.com/web-img/872/872b86faa7e3113776aae4de81ea961f.jpg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Chicken Potato Spinach Soup", "https://www.foodnetwork.com/recipes/chicken-potato-spinach-soup-3362502", "https://www.edamam.com/web-img/e56/e56bdee7d4b98c430ab96d5f614ef805.jpeg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 2, "Chicken, Potato And Vegetable Bake", "http://www.myrecipes.com/recipe/chicken-potato-and-vegetable-bake-191773/", "https://www.edamam.com/web-img/6be/6be5b6427fc7b70759e2dde21c606f3a.jpg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Baked Chicken, Potatoes, and Fennel recipes", "http://www.marthastewart.com/340625/baked-chicken-potatoes-and-fennel", "https://www.edamam.com/web-img/fdd/fdd1e47d25a0a3ccc5d0657c19faae37", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 2, "Buffalo Chicken Potato Skins recipes", "http://www.closetcooking.com/2012/02/buffalo-chicken-potato-skins.html", "https://www.edamam.com/web-img/d45/d45fa43d2864eaf1cd69537b698b00bb", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Roasted Chicken, Potatoes, and Fennel", "https://www.realsimple.com/food-recipes/browse-all-recipes/roasted-chicken-potatoes-fennel", "https://www.edamam.com/web-img/ed3/ed31c3f98057cdc69748798ff87ab9a6.jpg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Paprika-Roasted Chicken, Potatoes, and Radishes With Salsa Verde", "https://www.epicurious.com/recipes/food/views/paprika-roasted-chicken-potatoes-and-radishes-with-salsa-verde", "https://www.edamam.com/web-img/282/2825c398b109e6903d9a35b0e1ad1e2d.jpg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Skillet Chicken, Potatoes, and Peppers", "http://www.delish.com/cooking/recipe-ideas/recipes/a35651/skillet-chicken-potatoes-peppers-recipe-wdy0514/", "https://www.edamam.com/web-img/093/093219e1ceff0d98e4dba4323f30db0c.jpg", 5, NOW(), NOW());
+
+INSERT INTO favorite_recipes(user_id, title, url, image_url, rating, createdAt, updatedAt)
+Values ( 1, "Chicken, bacon & potato stew", "http://www.bbcgoodfood.com/recipes/1977648/chicken-bacon-and-potato-stew", "https://www.edamam.com/web-img/068/068647bd754e7fc667a770108972f27a.jpg", 5, NOW(), NOW());
