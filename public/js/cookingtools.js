75f948963af647af48d734992757a898e82f7f8e
G Dog <greg.hanstedt@med.ge.com>
2021-01-20 17:55:20 -0500

    Cleaned-up menu bar to handle Favorites properly with the dynamic URL

--- public/js/cookingtools.js
+++ public/js/cookingtools.js
@@ -312,3 +312,12 @@ function getTodos() {
       todoList.appendChild(todoDiv);
     });
   }
+
+  $(document).ready(function() {
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(function(data) {    // $(".member-name").text(data.email);
+      $("#favorite-link").attr(`href`,`/api/favorite/${data.id}`);
+    });
+  });
+  
--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+  $.get("/api/user_data").then(function(data) {    // $(".member-name").text(data.email);
+    $("#favorite-link").attr(`href`,`/api/favorite/${data.id}`);
   });
 
   $(".delete-favorite-button").on ("click", function (event) {
